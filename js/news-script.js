057edf2a1b26fe8e586431b2c0f8becd358e28ba
mikiqqqq <107814991+mikiqqqq@users.noreply.github.com>
2022-07-28 21:21:45 +0200

    Investing Webpage

---
+++ js/news-script.js
@@ -0,0 +1,81 @@
+var newsArticles = document.getElementsByClassName('other-item');
+var links = document.getElementsByClassName('other-link');
+var ct = 12;
+
+if(newsArticles.length > 12){
+  document.getElementById('read_more-button').style.display = 'inline-block';
+}
+
+document.getElementById('read_more-button').addEventListener('click', event => {
+
+  for(let i = ct; i < ct + 8; i++){
+    if(newsArticles.length - 1 == i){
+      newsArticles[i].style.display = 'block';
+      document.getElementById('read_more-button').style.display = 'none';
+      break;
+    }
+    newsArticles[i].style.display = 'block';
+  }
+
+  ct += 8;
+});
+
+function resetAllFiltersStyle(){
+  for(var i = 0; i < links.length; i++){
+    links[i].style.backgroundColor = "transparent";
+    links[i].style.color = "white";
+    links[i].style.pointerEvents = "auto";
+  }
+  document.getElementById('read_more-button').style.display = 'none';
+}
+
+function changeButtonStyle(filter){
+  filter.style.backgroundColor = "mintcream";
+  filter.style.color = "#cb0000";
+  filter.style.pointerEvents = "none";
+}
+
+function filterArticlesAndAppend(keyword){
+  for (var i = 0; i < newsArticles.length; i++) {
+    newsArticles[i].style.display = 'none';
+    var newsType = newsArticles[i].querySelector('.class').innerHTML;
+    if(newsType == keyword){
+      newsArticles[i].style.display = 'block';
+    }
+  }
+}
+
+var stocksFilter = document.getElementById('stocks-filter');
+stocksFilter.addEventListener('click', event => {
+  resetAllFiltersStyle();
+  changeButtonStyle(stocksFilter);
+  filterArticlesAndAppend('Stocks');
+});
+
+var cryptoFilter = document.getElementById('crypto-filter');
+cryptoFilter.addEventListener('click', event => {
+  resetAllFiltersStyle();
+  changeButtonStyle(cryptoFilter);
+  filterArticlesAndAppend('Crypto');
+});
+
+var bondsFilter = document.getElementById('bonds-filter');
+bondsFilter.addEventListener('click', event => {
+  resetAllFiltersStyle();
+  changeButtonStyle(bondsFilter);
+  filterArticlesAndAppend('Bonds');
+});
+
+var commFilter = document.getElementById('comm-filter');
+commFilter.addEventListener('click', event => {
+  resetAllFiltersStyle();
+  changeButtonStyle(commFilter);
+  filterArticlesAndAppend('Commodities');
+});
+
+var estateFilter = document.getElementById('estate-filter');
+estateFilter.addEventListener('click', event => {
+  resetAllFiltersStyle();
+  changeButtonStyle(estateFilter);
+  filterArticlesAndAppend('Real Estate');
+});
---
+++ js/script.js
@@ -0,0 +1,252 @@
+var screen_height = window.innerHeight;
+var screen_width = window.innerWidth;
+var banner_height, intro_height;
+
+var slider = document.getElementById('image-container');
+var sliderWidth = document.getElementById('slider').offsetWidth;
+var boxes = document.getElementsByClassName('box');
+ for (var i=0; i < boxes.length; i++) {
+   boxes[i].style.width = sliderWidth + "px";
+ }
+var leftAmount = sliderWidth;
+
+// Disable & Enable Scroll
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; }
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// Fade in/out
+let displayed = false;
+var button = document.getElementById('button');
+function appear(elm, i, step, speed){
+    var t_o;
+    //initial opacity
+    i = i || 0;
+    //opacity increment
+    step = step || 5;
+    //time waited between two opacity increments in msec
+    speed = speed || 50;
+
+    t_o = setInterval(function(){
+        //get opacity in decimals
+        var opacity = i / 100;
+        //set the next opacity step
+        i = i + step;
+        if(opacity > 1 || opacity < 0){
+            clearInterval(t_o);
+            //if 1-opaque or 0-transparent, stop
+            if(!displayed){
+              button.style.display = "none";
+            }
+            return;
+        }
+        //modern browsers
+        elm.style.opacity = opacity;
+        //older IE
+        elm.style.filter = 'alpha(opacity=' + opacity*100 + ')';
+    }, speed);
+}
+var counter = 0;
+
+function resize(){
+  if(window.availHeight == screen_height){
+    document.getElementById('banner').style.height = banner_height;
+  }
+  if(window.innerWidth != screen_width){
+    document.getElementById('banner').style.height = "fit-content";
+  }
+
+  AA = window.outerWidth;
+  BB = window.outerHeight;
+
+  if ((A == AA && B == BB) || BB >= B) {
+    screen_height = window.innerHeight;
+    banner_height = 0.90 * screen_height + "px";
+    intro_height = 0.10 * screen_height + "px";
+    document.getElementById('banner').style.height = banner_height;
+    document.getElementById('intro-quote').style.height = intro_height;
+
+    if(AA < A/2){
+      document.getElementById('banner').style.height = "fit-content";
+    }
+  }else{
+    displayed = false;
+    appear(button, 100, -3, 40);
+  }
+
+  if(!executing){
+  slider = document.getElementById('image-container');
+  sliderWidth = document.getElementById('slider').offsetWidth;
+  boxes = document.getElementsByClassName('box');
+   for (var i=0; i < boxes.length; i++) {
+     boxes[i].style.width = sliderWidth + "px";
+   }
+  leftAmount = sliderWidth;
+}
+}
+
+
+// Control scroll animation time
+const duration = 500;
+let scrolled = true;
+
+const scrollToTarget = function () {
+    scrolled = false;
+    disableScroll();
+    const top = window.innerHeight;
+
+
+    let startTime = null;
+    let requestId;
+
+    const loop = function (currentTime) {
+        if (!startTime) {
+            startTime = currentTime;
+        }
+
+        // Elapsed time in miliseconds
+        const time = currentTime - startTime;
+
+        const percent = Math.min(time / duration, 1);
+        window.scrollTo(0, top * percent);
+
+        if (time < duration) {
+            // Continue moving
+            requestId = window.requestAnimationFrame(loop);
+        } else {
+            window.cancelAnimationFrame(requestId);
+            scrolled = true;
+            enableScroll();
+        }
+    };
+    requestId = window.requestAnimationFrame(loop);
+};
+
+// Automatic News Slider
+function wait500ms(){
+  slider.style.transition = "left 2s";
+}
+
+function automaticSlider(){
+  executing = true;
+  if(counter == 4) {
+    slider.style.transition = "none";
+    counter= 0;
+    leftAmount = sliderWidth;
+    setTimeout(wait500ms, 500);
+  }
+  counter ++;
+  console.info("switching naturally " + counter);
+  leftAmount -= sliderWidth;
+  executing = false;
+  setTimeout(function(){
+    automaticSlider();
+  }, 5000);
+  slider.style.left = leftAmount + "px";
+}
+
+window.addEventListener('resize', resize);
+var A = screen.availWidth;
+var AA = window.outerWidth;
+
+var B = screen.availHeight;
+var BB = window.outerHeight;
+
+if ((A == AA && B == BB) || BB >= B) {
+  screen_height = window.innerHeight;
+  banner_height = 0.90 * screen_height + "px";
+  intro_height = 0.10 * screen_height + "px";
+  document.getElementById('banner').style.height = banner_height;
+  document.getElementById('intro-quote').style.height = intro_height;
+}else{
+  document.getElementById('intro-quote').style.height = "20vh";
+}
+
+window.addEventListener("load", () => {
+  var isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
+
+  if (isMobile) {
+    document.getElementById('banner').style.height = "100vh";
+    console.log("Is mobile device"); }
+  else { console.log("Not mobile device"); }
+});
+
+// Check if we're on the top of the page and height is max
+if(window.scrollY == 0 && BB >= B){
+  disableScroll();
+}
+
+// Check if read more was clicked
+document.getElementById('read').addEventListener("click", () => {
+  enableScroll();
+});
+
+// Track scroll amount
+var wheelScrolls = 0;
+window.addEventListener("wheel", event => {
+    const delta = Math.sign(event.deltaY);
+    if(delta == 1){
+      wheelScrolls++;
+      console.info("scrolls: " + wheelScrolls);
+      if(wheelScrolls >= 3 && window.scrollY == 0){
+        if(scrolled) scrollToTarget();
+      }
+    }
+    console.info(delta);
+});
+
+automaticSlider();
+
+// Back to top button
+button.style.display = "none";
+button.style.opacity = "0";
+window.addEventListener('scroll', function () {
+    if(window.scrollY >= 2*window.innerHeight && displayed == false && (AA >= A/2 && BB>=B)){
+      displayed = true;
+      button.style.display = "block";
+      appear(button, 0, 3, 40);
+    }
+    if(window.scrollY < 2*window.innerHeight && displayed == true){
+      displayed = false;
+      appear(button, 100, -3, 40);
+    }
+});
